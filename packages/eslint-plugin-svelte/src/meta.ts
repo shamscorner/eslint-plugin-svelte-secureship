a16a5d73a6e4a0ea6efda0c47e894e1ab73d13c7
Shamim <hossains159@gmail.com>
2025-01-03 19:46:19 +0600

    chore: update package name

--- packages/eslint-plugin-svelte/src/meta.ts
+++ packages/eslint-plugin-svelte/src/meta.ts
@@ -1,5 +1,5 @@
 // IMPORTANT!
 // This file has been automatically generated,
 // in order to update its content execute "pnpm run update"
-export const name = '@secureship/eslint-plugin-svelte';
+export const name = '@shamscorner/eslint-plugin-svelte';
 export const version = '3.0.0-next.10';
